4854ad059253b516bdab4efbda57c34c49f01f98
bishalamgai <Bishal.Amgai@triosstudent.com>
2024-03-21 15:32:37 -0400

    first commit

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello! I'm Bishal Amgai."
+let rec factorial number accumalotor =
+    if number = 0 then
+        accumalotor
+    else
+        factorial (number - 1) (accumalotor * number)
+
+let result = factorial 7 1
+printfn "Factorial of 7 is: %d" result
+
